741862d6397b7837674ce026553d746447296509
Andres Dejesus <andresdejesus123@gmail.com>
2023-09-02 18:59:19 -0500

    input reading, bg color

--- src/bin/snake.rs
+++ src/bin/snake.rs
@@ -2,7 +2,8 @@ use anyhow::Context;
 use clap::Parser;
 
 const TITLE: &str = "Snake";
-const DEFAULT_FPS: u32 = 2;
+const BG_COLOR: (u8, u8, u8) = (145, 230, 255);
+const DEFAULT_FPS: u32 = 5;
 
 #[derive(Parser)]
 struct Cli {
@@ -13,10 +14,10 @@ struct Cli {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
-    let config = game::Config::new(TITLE.into(), cli.fps.unwrap_or(DEFAULT_FPS))
+    let config = game::Config::new(TITLE.into(), BG_COLOR, cli.fps.unwrap_or(DEFAULT_FPS))
         .context("parsing command line arguments")?;
 
-    game::render(config).await.context("rendering snake game")?;
+    game::init(config).await.context("rendering snake game")?;
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use crossterm::{
-    event::{DisableMouseCapture, EnableMouseCapture},
+    event::{DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyModifiers},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
+use futures::StreamExt;
 use ratatui::{
     backend::CrosstermBackend,
     prelude::*,
@@ -11,12 +12,14 @@ use ratatui::{
 };
 use std::{io, ops::RangeInclusive, time::Duration};
 use thiserror::Error;
+use tokio::{join, time};
 
-const FPS_BOUNDS: RangeInclusive<u32> = 1..=5;
+const FPS_BOUNDS: RangeInclusive<u32> = 1..=10;
 
 /// Configuration settings for the game.
 pub struct Config {
     title: String,
+    bg_color: (u8, u8, u8),
     fps: u32,
 }
 
@@ -34,7 +37,7 @@ pub enum GameError {
 }
 
 impl Config {
-    pub fn new(title: String, fps: u32) -> Result<Self, GameError> {
+    pub fn new(title: String, bg_color: (u8, u8, u8), fps: u32) -> Result<Self, GameError> {
         if !FPS_BOUNDS.contains(&fps) {
             return Err(GameError::BadArg(format!(
                 "fps must be between {} and {}",
@@ -43,7 +46,7 @@ impl Config {
             )));
         }
 
-        Ok(Self { title, fps })
+        Ok(Self { title, bg_color, fps })
     }
 }
 
@@ -70,7 +73,7 @@ impl RenderHandle {
             Ok(terminal) => terminal,
             Err(error) => {
                 disable_raw_mode()?;
-                return Err(GameError::Io(error));
+                return Err(error.into());
             }
         };
 
@@ -95,29 +98,56 @@ impl Drop for RenderHandle {
     }
 }
 
+enum Input {
+    None,
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 /// Begin rendering the game using the provided `config` settings.
-pub async fn render(config: Config) -> Result<(), GameError> {
+pub async fn init(config: Config) -> Result<(), GameError> {
     let mut handle = RenderHandle::new()?;
     let terminal = &mut handle.terminal;
 
+    let mut stream = crossterm::event::EventStream::new();
     let sleep_duration = Duration::from_secs_f32(1_f32 / config.fps as f32);
 
+    let color = Color::Rgb(config.bg_color.0, config.bg_color.1, config.bg_color.2);
     let game_widget = Block::default()
         .title(format!(" {} ", config.title))
         .title_style(Style::default().add_modifier(Modifier::BOLD))
         .title_alignment(Alignment::Center)
         .borders(Borders::ALL)
-        .border_type(BorderType::Thick);
+        .border_type(BorderType::Thick)
+        .style(Style::default().bg(color));
 
-    for _ in 0..20 {
+    loop {
         terminal.draw(|frame| {
             frame.render_widget(game_widget.clone(), frame.size());
         })?;
 
-        tokio::time::sleep(sleep_duration).await;
-    }
+        // future returns every single sleep_duration, and a key press might be returned
+        if let (Ok(Some(result)), _) = join!(
+            time::timeout(sleep_duration, stream.next()),
+            time::sleep(sleep_duration)
+        ) {
+            if let Event::Key(key) = result? {
+                if key.modifiers.contains(KeyModifiers::CONTROL) && key.code == KeyCode::Char('c') {
+                    return Ok(());
+                }
 
-    Ok(())
+                let _input = match key.code {
+                    KeyCode::Up => Input::Up,
+                    KeyCode::Down => Input::Down,
+                    KeyCode::Left => Input::Left,
+                    KeyCode::Right => Input::Right,
+                    _ => Input::None,
+                };
+            }
+        }
+    }
 }
 
 #[cfg(test)]
